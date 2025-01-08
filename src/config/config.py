92db79eee5edf6642cb209e4b940b102a7b3996d
Aniket <101495282+aniketgit2110@users.noreply.github.com>
2025-01-08 11:52:41 +0530

    Add files via upload

---
+++ src/config/config.py
@@ -0,0 +1,18 @@
+config = {
+    "gemini": {
+        "api_key": "AIzaSyBXPkjm3aSTVsmAUIXymH6SxXmr6NHWv44"
+    },
+    "kafka": {
+        "sasl.username": "W3MVBYNET37L746V",
+        "sasl.password": "2Zm++59eJZT5Yq+y7lOu9aGWFUpGu1rL2aaGaP73owHv8b1VgfSTpd0DTcTUH8C6",
+        "bootstrap.servers": "pkc-12576z.us-west2.gcp.confluent.cloud:9092",
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanisms': 'PLAIN',
+        'session.timeout.ms': 50000
+    },
+    "schema_registry": {
+        "url": "TDPHD3XFOINICVGA",
+        "basic.auth.user.info": "TDPHD3XFOINICVGA:H6aQptrE2GU6degK+ItIHpXakgCmCOw4HSAbbR6f1TK9vbTqXSNRKCF1OPNVON+O"
+
+    }
+}
